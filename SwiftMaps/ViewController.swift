7b4c4f484b0c13daaed6d9691ca10cbcf6f0ef3a
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-08-29 20:46:57 +0300

    Getting user location

--- SwiftMaps/ViewController.swift
+++ SwiftMaps/ViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , MKMapViewDelegate, CLLocationManagerDelegate{
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager=CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate=self
+        mapView.delegate = self
+        
+        locationManager.desiredAccuracy=kCLLocationAccuracyBestForNavigation
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        
+        let span=MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region=MKCoordinateRegion(center:location,span:span)
+        mapView.setRegion(region, animated: true)
+        
+        // NOTE: locations[0] brings user's last location
     }
 
 
